4c2ca011418b4aca9ec3b0ee55599367b6ef43eb
Lucas Dos Santos Vellido <lucas@ampliato.com.br>
2022-01-18 22:11:30 -0300

    - Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Agribusiness marketplace')
+    .setDescription('Connecting your family with others')
+    .setVersion('1.0')
+    // .addTag('cbc')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   app.setGlobalPrefix('api/v1');
   await app.listen(3000, '0.0.0.0');
 }
